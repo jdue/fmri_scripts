3d168482467a0a0d5d3a48c79cf4372297ae66f1
Jesper Duemose Nielsen <jesperdn@drcmr.dk>
2017-07-19 19:11:03 +0200

    removed misc

--- fs_defaults.m
+++ fs_defaults.m
@@ -3,7 +3,6 @@ function fs_defaults()
 fd = fs_dir();
 
 addpath(fullfile(fd, 'glm'));
-addpath(fullfile(fd, 'misc'));
 addpath(fullfile(fd, 'preproc'));
 addpath(fullfile(fd, 'utils'));
 addpath(fullfile(fd, 'utils_generic'));
